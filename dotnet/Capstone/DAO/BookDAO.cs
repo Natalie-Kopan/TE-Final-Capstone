9a554bd6b3c8287dae2a5527726c869ab1fd5118
William Randolph <williamrandolphiii@gmail.com>
2021-12-05 20:34:56 -0500

    Capstone Add Book

---
+++ dotnet/Capstone/DAO/BookDAO.cs
@@ -0,0 +1,42 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.DAO
+{
+    public class BookDAO : IBookDAO
+    {
+        private const string AddBooksSql = "INSERT INTO books (isbn, title, author) VALUES (@isbn, @title, @author)";
+
+        private readonly string connectionString;
+
+        public BookDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Book AddBook(Book bookToAdd)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand command = new SqlCommand(AddBooksSql, conn))
+                {
+                  
+                    command.Parameters.AddWithValue("@isbn", bookToAdd.ISBN);
+                    command.Parameters.AddWithValue("@title", bookToAdd.Title);
+                    command.Parameters.AddWithValue("@author", bookToAdd.Author);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return bookToAdd;
+        }
+
+    }
+}
