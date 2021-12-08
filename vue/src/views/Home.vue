faf497fcbf9d4369fc175fc3c9fcc6775b0e3fcf
William Randolph <williamrandolphiii@gmail.com>
2021-12-07 19:36:46 -0500

    Breakfast Club

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>Your content goes here. Make it look nice.</p>
+    <h1>Welcome To The Breakfast Club</h1>
+    <p>Launched in 1984, <em>The Breakfast Club</em> became an inclusive book club amongst aspiring Tech Elevator classmates. 
+      While the concept of <em>The Breakfast Club</em> began as a study hall, it quickly grew into a fun-filled reading competition that now their kids can enjoy.</p>
   </div>
 </template>
 
