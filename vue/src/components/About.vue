12b834b0c26859afb4ac2304697c78fbe411c139
William Randolph <williamrandolphiii@gmail.com>
2021-12-13 18:23:04 -0500

    william

--- vue/src/components/About.vue
+++ vue/src/components/About.vue
@@ -1,5 +1,9 @@
 <template>
-    <p>
+ <div>
+  <img src="../../img/istockphoto-1138365810-170667a.jpg" alt="kids reading" /> 
+
+  <img src="../../img/istockphoto-1294218659-170667a.jpg" alt="parent reading to kids" />
+   <p>
       Launched in 1984, <em>The Breakfast Book Club</em> became an inclusive
       book club amongst aspiring Tech Elevator classmates. While the concept of
       <em>The Breakfast Club</em> began as a study hall, it quickly grew into a
@@ -7,6 +11,8 @@
       their kids where they can track daily reading, compete with friends and
       family to get prizes and continue the traditon of the club.
     </p>
+    
+ </div>
 </template>
 
 <script>
@@ -16,5 +22,24 @@ export default {
 </script>
 
 <style>
+
+p {
+  margin: 20px;
+  padding: 50px;
+  font-size: 30px;
+  align-items: center;
+}
+
+#nav {
+  margin: 20px;
+}
+
+img {
+  align-items: center;
+  padding: 40px;
+  margin: 10px;
+  size: 50%;
+  
+}
 
 </style>
